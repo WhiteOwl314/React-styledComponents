f8edcf5ee9da406efb02d6e85dbe4b12321eabdb
ParkSeongju <parksj914@naver.com>
2020-08-27 02:05:14 +0900

    palette 리팩토링2

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,49 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import {lighten, darken} from "polished";
 
+const colorStyles = css`
+    ${({theme, color})=>{
+        const selected = theme.palette[color];
+        return css`
+            background: ${selected};
+            &:hover{
+                background: ${lighten(0.1,selected)};
+            }
+            &:active{
+                background: ${darken(0.1,selected)};
+            }
+        `;
+    }}
+`;
+
+const sizeStyles = css`
+    ${props =>
+        props.size === 'large' &&
+        css`
+            height: 3rem;
+            font-size: 1.25rem;
+        `
+    }
+    
+    ${props =>
+        props.size === 'medium' &&
+        css`
+            height: 2.25rem;
+            font-size: 1rem;
+        `
+    }
+    
+    ${props =>
+        props.size === 'small' &&
+        css`
+            height: 1.75rem;
+            font-size: 0.875rem;
+        `
+    }
+    
+    
+`;
+
 const StyledButton = styled.button`
     /*공통 스타일*/
     display: inline-flex;
@@ -15,22 +58,10 @@ const StyledButton = styled.button`
     padding-right: 1rem;
     
     /*크기*/
-    height: 2.25rem;
-    font-size: 1rem;
+    ${sizeStyles}
     
     /*색상*/
-    ${({theme, color}) =>{
-        const selected = theme.palette[color];
-        return css`
-            background: ${selected};
-            &:hover{
-                background: ${lighten(0.1,selected)};
-            }
-            &:active{
-                background: ${darken(0.1,selected)};
-            }
-        `
-    }}
+    ${colorStyles}
     
     /*기타*/
     & + &{
